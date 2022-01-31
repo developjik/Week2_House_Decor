b85c331ea5c51bb8a9e3b7801662fff0dd8f4b0f
developjik <developjik@gmail.com>
2022-01-31 19:14:59 +0900

    chore: 파일 이름 수정

--- src/Pages/HouseIntroduction.tsx
+++ src/Pages/HouseIntroduction.tsx
@@ -1,10 +1,10 @@
+/* eslint-disable max-len */
 import React from "react";
-import Title from "Components/HouseIntroduction/Title";
-import Content from "Components/HouseIntroduction/Content";
+import HomeIntroTitle from "Components/HouseIntroduction/HouseIntroTitle";
+import HomeIntroContent from "Components/HouseIntroduction/HouseIntroContent";
+import "Pages/Scss/HouseIntroduction.scss";
 
 import { titleDatasType } from "Common/Types/types";
-
-import "Pages/Scss/HouseIntroduction.scss";
 
 const titleDatas: titleDatasType = {
   title: "화이트, 우드, 라탄! 세 가지 컨셉으로 꾸민 내 방",
@@ -18,8 +18,8 @@ function HouseIntroduction() {
   return (
     <div className="home-intro-container">
       <div className="home-intro">
-        <Title titleDatas={titleDatas} />
-        <Content />
+        <HomeIntroTitle titleDatas={titleDatas} />
+        <HomeIntroContent />
       </div>
     </div>
   );
